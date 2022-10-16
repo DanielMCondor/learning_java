3f940ed1adc7167365c866f1191efa38f5e75bb4
DanielMCondor <dmantillacondor@gmail.com>
2022-10-16 11:31:00 -0500

    use arithmetic operators

---
+++ src/arithmeticOperators.java
@@ -0,0 +1,28 @@
+public class arithmeticOperators {
+    public static void main(String [] args) {
+        // TODO: 2022-10-16 variables
+        double firstNumber = 30;
+        double secondNumber = 15;
+        // TODO: 2022-10-16 add
+        double add = firstNumber + secondNumber;
+        // TODO: 2022-10-16 subtract
+        double subtract = firstNumber - secondNumber;
+        // TODO: 2022-10-16 multiply
+        double multiply = firstNumber * secondNumber;
+        // TODO: 2022-10-16 divide
+        double divide = firstNumber / secondNumber;
+        // TODO: 2022-10-16 remainder
+        double remainder = firstNumber % secondNumber;
+
+        System.out.println("ARITHMETIC OPERATORS");
+        System.out.println("======================");
+        System.out.println("firs number..: " + firstNumber);
+        System.out.println("second Number: " + secondNumber);
+        System.out.println("...................");
+        System.out.println("add..........: " + add);
+        System.out.println("substract....: "+ subtract);
+        System.out.println("multiply.....: " + multiply);
+        System.out.println("divide.......: "+ divide);
+        System.out.println("resto........: "+ remainder);
+    }
+}
